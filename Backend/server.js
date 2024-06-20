643320e1fdfdc7643d744270e2f3d2f2e8e0215f
unknown <darshanjadhav775@gmail.com>
2024-06-20 12:24:15 +0530

    enable Cors

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ db.on('connected', () => {
 });
 const io = require("socket.io")(3001, {
     cors: {
-        origin: "http://localhost:5173",
+        origin: ["http://localhost:5173", "https://all-editor-mf5q.vercel.app"],
         methods: ["GET", "POST"],
     },
 });
